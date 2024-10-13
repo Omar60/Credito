72fbcfce08f0fe3f16efefe2b829a6601dfdf737
iDevotion <omar60@live.com.mx>
2024-10-13 16:22:55 -0600

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -78,7 +78,7 @@ const App: React.FC = () => {
 
   return (
     <div className="min-h-screen bg-gray-100 py-6 flex flex-col justify-center sm:py-12">
-      <div className="relative py-3 sm:max-w-xl md:max-w-2xl lg:max-w-5xl xl:max-w-7xl mx-auto w-full px-4">
+      <div className="relative py-3 sm:max-w-xl md:max-w-3xl lg:max-w-5xl xl:max-w-7xl mx-auto w-full px-4">
         <div className="absolute inset-0 bg-gradient-to-r from-cyan-400 to-light-blue-500 shadow-lg transform -skew-y-6 sm:skew-y-0 sm:-rotate-6 sm:rounded-3xl"></div>
         <div className="relative px-4 py-10 bg-white shadow-lg sm:rounded-3xl sm:p-20">
           <div className="max-w-full mx-auto">
